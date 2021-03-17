27374a33403190775e14eee17a609fb3eff5d589
Hugo <hqtello@gmail.com>
2021-03-17 17:01:20 -0500

    Made the Dashboard Drawer component responsive

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Button, Icon, Drawer } from 'rsuite'
 
-import { useModalState } from '../../misc/custom-hooks'
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks'
 import Dashboard from './Dashboard'
 
 function DashboardToggle() {
     const { isOpen, open, close } = useModalState()
+    const isMobile = useMediaQuery('(max-width: 992px)')
 
     return (
         <>
             <Button block color='blue' onClick={open}>
                 <Icon icon='dashboard' /> Dashboard
             </Button>
-            <Drawer show={isOpen} onHide={close} placement='left'>
+            <Drawer
+                full={isMobile}
+                show={isOpen}
+                onHide={close}
+                placement='left'
+            >
                 <Dashboard />
             </Drawer>
         </>
