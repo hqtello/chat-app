4d582db69e108f138ac0cf3c8f5ccecbf6d03a6d
Hugo <hqtello@gmail.com>
2021-03-26 11:56:35 -0500

    Added real-time presence to listener

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { Button, Icon, Drawer, Alert } from 'rsuite'
+import React from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
 
-import { useModalState, useMediaQuery } from '../../misc/custom-hooks'
-import Dashboard from './Dashboard'
-import { auth } from '../../misc/firebase'
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
+import Dashboard from './Dashboard';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 function DashboardToggle() {
-    const { isOpen, open, close } = useModalState()
-    const isMobile = useMediaQuery('(max-width: 992px)')
+  const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 992px)');
 
-    const handleSingOut = async () => {
-        await auth.signOut()
+  const handleSingOut = () => {
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
 
-        Alert.info('Signed out', 4000)
+        Alert.info('Signed out', 4000);
 
         // close the dashboard drawer
-        close()
-    }
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
+  };
 
-    return (
-        <>
-            <Button block color='blue' onClick={open}>
-                <Icon icon='dashboard' /> Dashboard
-            </Button>
-            <Drawer
-                full={isMobile}
-                show={isOpen}
-                onHide={close}
-                placement='left'
-            >
-                <Dashboard
-                    onSignOut={handleSingOut}
-                />
-            </Drawer>
-        </>
-    )
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard onSignOut={handleSingOut} />
+      </Drawer>
+    </>
+  );
 }
 
-export default DashboardToggle
+export default DashboardToggle;
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,5 +1,21 @@
+/* eslint-disable */
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+
 import { auth, database } from '../misc/firebase';
+
+// We'll create two constants which we will write to
+// the Realtime database when this device is offline
+// or online.
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const ProfileContext = createContext();
 
@@ -9,10 +25,14 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
+    let userStatusRef;
 
     // allows us to subscribe to currently sign-in users
     const authUnsubscribe = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        // Create a reference to this user's specific status node.
+        // This is where we will store data about being online/offline.
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
 
         userRef.on('value', snapshot => {
@@ -29,10 +49,42 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setIsLoading(false);
         });
+
+        // Create a reference to the special '.info/connected' path in
+        // Realtime Database. This path returns `true` when connected
+        // and `false` when disconnected.
+        database.ref('.info/connected').on('value', snapshot => {
+          // If we're not currently connected, don't do anything.
+          if (snapshot.val() === false) {
+            return;
+          }
+
+          // If we are currently connected, then use the 'onDisconnect()'
+          // method to add a set which will only trigger once this
+          // client has disconnected by closing the app,
+          // losing internet, or any other means.
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              // The promise returned from .onDisconnect().set() will
+              // resolve as soon as the server acknowledges the onDisconnect()
+              // request, NOT once we've actually disconnected:
+              // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
+
+              // We can now safely set ourselves as 'online' knowing that the
+              // server will mark us as offline once we lose connection.
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef)
           //  unsubscribe from the .on() above
           userRef.off();
+
+        if (userStatusRef) userStatusRef.off();
+
+        database.ref('.info/connected').off();
 
         setProfile(null);
         setIsLoading(false);
@@ -44,6 +96,10 @@ export const ProfileProvider = ({ children }) => {
       authUnsubscribe();
 
       if (userRef) userRef.off();
+
+      if (userStatusRef) userStatusRef.off();
+
+      database.ref('.info/connected').off();
     };
   }, []);
 
